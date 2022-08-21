1146490d2e3803bd6bbfcbc21e2994f41da184b6
HashHaran <hariharan@alumni.iitm.ac.in>
2022-08-21 08:12:41 +0530

    #blockchaiin #unindexing #contentId - Gave an extra parameter to the event as content id string without indexing so it does not get hashed

--- blockchain/contracts/ISovereignity.sol
+++ blockchain/contracts/ISovereignity.sol
@@ -2,17 +2,60 @@
 pragma solidity ^0.8.9;
 
 interface ISovereignity {
-    event ContentCreation(string indexed contentId, address indexed owner, uint timeStamp);
-    event PermissionGranted(string indexed contentId, address indexed owner, address indexed user, uint timeStamp);
-    event PermissionRevoked(string indexed contentId, address indexed owner, address indexed user, uint timeStamp);
-    event ContentDeletion(string indexed contentId, address indexed owner, uint timeStamp);
-    event OwnershipTransfered(string indexed contentId, address indexed owner, address indexed newOwner, uint timeStamp);
+    event ContentCreation(
+        string indexed contentId,
+        string contentIdString,
+        address indexed owner,
+        uint256 timeStamp
+    );
+    event PermissionGranted(
+        string indexed contentId,
+        string contentIdString,
+        address indexed owner,
+        address indexed user,
+        uint256 timeStamp
+    );
+    event PermissionRevoked(
+        string indexed contentId,
+        string contentIdString,
+        address indexed owner,
+        address indexed user,
+        uint256 timeStamp
+    );
+    event ContentDeletion(
+        string indexed contentId,
+        string contentIdString,
+        address indexed owner,
+        uint256 timeStamp
+    );
+    event OwnershipTransfered(
+        string indexed contentId,
+        string contentIdString,
+        address indexed owner,
+        address indexed newOwner,
+        uint256 timeStamp
+    );
 
     function ownerOf(string calldata contentId) external view returns (address);
+
     function addContent(string calldata contentId) external;
-    function deleteContent(string calldata contentId, address[] calldata permUsers) external;
+
+    function deleteContent(
+        string calldata contentId,
+        address[] calldata permUsers
+    ) external;
+
     function awardPermission(string calldata contentId, address user) external;
+
     function revokePermission(string calldata contentId, address user) external;
-    function isPermitted(string calldata contentId, address user) external returns (bool);
-    function transferOwnership(string calldata contentId, address newOwner, address[] calldata permUsers) external; 
+
+    function isPermitted(string calldata contentId, address user)
+        external
+        returns (bool);
+
+    function transferOwnership(
+        string calldata contentId,
+        address newOwner,
+        address[] calldata permUsers
+    ) external;
 }
